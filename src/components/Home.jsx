7603b4a9082ea6748141232484840a0086bf2201
Seyi0 <makanjuolaseyi560@gmail.com>
2022-08-18 13:40:14 +0100

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   return (
     <>
       <div className="p-4 box mt-3 text-center">
-        Hello Welcome
+      
         This an authorization app
          <br />
         {user && user.email}
